4885c1853f1bca59bca0dafafe73ba2a4ff714e8
Michel Ticiani <michel.ticiani@appsistemas.com.br>
2022-09-03 03:45:07 -0300

    Add project files.

---
+++ Has.Duplicates/IEnumerableExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Has.Duplicates
+{
+    public static class IEnumerableExtensions
+    {
+        public static int DuplicatesCount<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.Duplicates(x => x).Count;
+        }
+
+        public static List<TSource> Duplicates<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.Duplicates(x => x);
+        }
+
+        public static List<TSource> Duplicates<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        {
+            return source.GroupBy(selector).Where(x => x.IsMultiple()).SelectMany(x => x).ToList();
+        }
+
+        public static bool IsMultiple<TSource>(this IEnumerable<TSource> source)
+        {
+            var enumerator = source.GetEnumerator();
+            return enumerator.MoveNext() && enumerator.MoveNext();
+        }
+
+        public static List<TSource> DistinctList<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.Distinct().ToList();
+        }
+
+        public static List<TSource> DistinctList<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        {
+            return source.DistinctBy(selector).ToList();
+        }
+
+        public static List<TSource> OrderByList<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.OrderBy(x => x).ToList();
+        }
+
+        public static List<TSource> OrderByList<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        {
+            return source.OrderBy(selector).ToList();
+        }
+
+        public static List<TSource> OrderByDescendingList<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.OrderByDescending(x => x).ToList();
+        }
+
+        public static List<TSource> OrderByDescendingList<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        {
+            return source.OrderByDescending(selector).ToList();
+        }
+    }
+}
---
+++ Has.Duplicates/Program.cs
@@ -0,0 +1,27 @@
+﻿using Has.Duplicates;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new List<int> { 1, 2, 3 };
+        var listDup = new List<int> {
+            1,
+            2,
+            3, 3, 3, 3,
+            4,
+            5, 5,
+            6,
+            7,
+            8,
+            9,
+            0, 0 };
+
+        Console.WriteLine($"Número de itens duplicados: {list.DuplicatesCount()}");
+        list.Duplicates().DistinctList().OrderByList().ForEach(x => Console.WriteLine($"Item duplicado: {x}"));
+
+        Console.WriteLine($"Número de itens duplicados: {listDup.DuplicatesCount()}");
+        listDup.Duplicates().DistinctList().OrderByList().ForEach(x => Console.WriteLine($"Item duplicado: {x}"));
+    }
+}
+
